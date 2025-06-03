aa17423b4150fd7d2ce57e65b709f182ded4f392
Ahkeem <langahkeem@gmail.com>
2025-06-03 17:53:42 -0500

    resolved dependency conflicts and tailwind config issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 module.exports = {
     darkMode: ["class"],
     content: [
-      "./pages/**/*.{ts,tsx}",
-      "./components/**/*.{ts,tsx}",
-      "./app/**/*.{ts,tsx}",
-      "./src/**/*.{ts,tsx}",
-      "*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     prefix: "",
     theme: {
@@ -76,4 +75,3 @@ module.exports = {
     },
     plugins: [require("tailwindcss-animate")],
   }
-  
